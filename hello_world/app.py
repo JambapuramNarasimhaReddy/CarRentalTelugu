4d146128f3db107156e760e5f0f3aa0e9567a9b2
JAMBAPURAM NARASIMHA REDDY <71832765+JambapuramNarasimhaReddy@users.noreply.github.com>
2025-02-05 18:59:30 +0530

    Update app.py

--- hello_world/app.py
+++ hello_world/app.py
@@ -7,6 +7,7 @@ def lambda_handler(event, context):
     print(event)
     print("task Update")
     print("Sample Update")
+    print("Locla Update")
     return {
         "statusCode": 200,
         "body": json.dumps({
